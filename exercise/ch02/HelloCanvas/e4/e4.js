755c7d06be2edd216d0111079cc4f49ee73de1a1
wuyutaott <wuyutaoktm@163.com>
2021-12-25 07:55:12 +0800

    HelloCanvas e4

---
+++ exercise/ch02/HelloCanvas/e4/e4.js
@@ -0,0 +1,10 @@
+function main() {
+    const canvas = document.createElement('canvas');
+    canvas.width = 400;
+    canvas.height = 400;
+    document.getElementsByTagName('body')[0].appendChild(canvas);
+
+    const gl = canvas.getContext('webgl');
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
