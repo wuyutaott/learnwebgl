3a993a530fb60c31ad2b38e2b8c494f77a079d98
wuyutaott <wuyutaoktm@163.com>
2021-12-27 08:50:45 +0800

    ClickedPoints e1

--- exercise/ch02/ClickedPoints/e1/e1.js
+++ exercise/ch02/ClickedPoints/e1/e1.js
@@ -8,9 +8,10 @@ function main() {
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    const VS = `void main() {
-                    gl_Position = vec4(0, 0, 0, 1);
-                    gl_PointSize = 10.0;
+    const VS = `attribute vec4 a_Position;
+                void main() {
+                    gl_Position = a_Position;
+                    gl_PointSize = 5.0;
                 }`;
     const FS = `void main() {
                     gl_FragColor = vec4(1, 0, 0, 1);
@@ -28,14 +29,27 @@ function main() {
     gl.attachShader(program, vshader);
     gl.attachShader(program, fshader);
     gl.linkProgram(program);
-    gl.useProgram(program);
+    gl.useProgram(program);  
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    var a_Position = gl.getAttribLocation(program, 'a_Position');
 
+    var points = [];
     canvas.onmousedown = (ev) => {
         var rect = canvas.getBoundingClientRect();        
         var clickX = ev.clientX - rect.left;
         var clickY = ev.clientY - rect.top;
-        console.log(clickX, clickY);
+        var xx = (clickX - rect.width / 2) / (rect.width / 2);
+        var yy = (rect.height / 2 - clickY) / (rect.height / 2);   
+        points.push(xx);
+        points.push(yy);     
+        console.log(points);
+
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        for (var i = 0; i < points.length; i += 2) {
+            var x = points[i];
+            var y = points[i+1];
+            gl.vertexAttrib3f(a_Position, x, y, 0);
+            gl.drawArrays(gl.POINTS, 0, 1);
+        }
     };
 }
