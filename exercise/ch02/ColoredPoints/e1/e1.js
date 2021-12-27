7641fb852cdaaa982ebc141773a5daed6f12b4f0
wuyutaott <wuyutaoktm@163.com>
2021-12-28 07:23:46 +0800

    ColorPoints e1 2.鼠标点击的地方画点

--- exercise/ch02/ColoredPoints/e1/e1.js
+++ exercise/ch02/ColoredPoints/e1/e1.js
@@ -6,8 +6,9 @@ function main() {
 
     const gl = canvas.getContext('webgl');
 
-    const VS = `void main() {
-                    gl_Position = vec4(0, 0, 0, 1);
+    const VS = `attribute vec4 a_Position;
+                void main() {
+                    gl_Position = a_Position;
                     gl_PointSize = 10.0;
                 }`;
     const FS = `void main() {
@@ -29,6 +30,25 @@ function main() {
     gl.useProgram(program);
 
     gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);    
+
+    var a_Position = gl.getAttribLocation(program, 'a_Position');
+
+    var points = [];
+    canvas.onmousedown = (ev) => {
+        var rect = canvas.getBoundingClientRect();
+        var cx = ev.clientX - rect.left;
+        var cy = ev.clientY - rect.top;
+        var x = (cx - rect.width / 2) / (rect.width / 2);
+        var y = (rect.height / 2 - cy) / (rect.height / 2);
+        points.push([x, y]);
+
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        
+        for (var i = 0; i < points.length; i++) {
+            var p = points[i];
+            gl.vertexAttrib3f(a_Position, p[0], p[1], 0);
+            gl.drawArrays(gl.POINTS, 0, 1);
+        }
+    }
 }
