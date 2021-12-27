ae5a554fd73de43d58b21c94b071f6765482a473
wuyutaott <wuyutaoktm@163.com>
2021-12-28 05:06:45 +0800

    HelloPoint2 e4

---
+++ exercise/ch02/HelloPoint2/e4/e4.js
@@ -0,0 +1,31 @@
+function main() {
+    const canvas = document.createElement('canvas');
+    canvas.width = 300;
+    canvas.height = 300;
+    document.getElementsByTagName('body')[0].appendChild(canvas);
+
+    const gl = canvas.getContext('webgl');
+
+    const VS = `void main() {
+                    gl_Position = vec4(0, 0, 0, 1);
+                    gl_PointSize = 10.0;
+                }`;
+    const FS = `void main() {
+                    gl_FragColor = vec4(1, 0, 0, 1);
+                }`;
+
+    var vshader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vshader, VS);
+    gl.compileShader(vshader);
+    var fshader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fshader, VS);
+    gl.compileShader(fshader);
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vshader);
+    gl.attachShader(program, fshader);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
