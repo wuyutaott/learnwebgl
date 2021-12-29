db77cb3f5b0c806a9fd3e5d1ddd6459180cf2b92
wuyutaott <wuyutaoktm@163.com>
2021-12-30 07:59:08 +0800

    MultiPoints e2

---
+++ exercise/ch03/MultiPoints/e2/e2.ts
@@ -0,0 +1,50 @@
+function main() {
+    const canvas = document.createElement('canvas');
+    canvas.width = 300;
+    canvas.height = 300;
+    document.getElementsByTagName('body')[0].appendChild(canvas);
+
+    const gl = canvas.getContext('webgl');
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const VS = `attribute vec4 a_Position;
+                void main() {
+                    gl_Position = a_Position;
+                    gl_PointSize = 5.0;
+                }`;
+    const FS = `void main() {
+                    gl_FragColor = vec4(1, 0, 0, 1);
+                }`;
+    
+    let vshader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vshader, VS);
+    gl.compileShader(vshader);
+
+    let fshader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fshader, FS);
+    gl.compileShader(fshader);
+
+    let program = gl.createProgram();
+    gl.attachShader(program, vshader);
+    gl.attachShader(program, fshader);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+
+    // let a_Position = gl.getAttribLocation(program, 'a_Position');
+    // gl.vertexAttrib3f(a_Position, 0, 0, 0);
+
+    let num = createBuffer(gl, program);
+    gl.drawArrays(gl.POINTS, 0, num);
+}
+
+function createBuffer(gl: WebGLRenderingContext, program: WebGLProgram): number {
+    let data = new Int32Array([0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+    let buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+    let a_Position = gl.getAttribLocation(program, 'a_Position');
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+    return 3;
+}
