91ccf65d19c8d58e78653ea1dda59c8a897ce908
wuyutaott <wuyutaoktm@163.com>
2022-01-11 06:33:36 +0800

    RotatingTriangle e1

---
+++ exercise/ch04/RotatingTriangle/e1/e1.js
@@ -0,0 +1,49 @@
+function main() {
+    const canvas = document.createElement('canvas');
+    canvas.width = 300;
+    canvas.height = 300;
+    document.getElementsByTagName('body')[0].appendChild(canvas);
+
+    const gl = canvas.getContext('webgl');
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const VS = `attribute vec4 a_Position;
+                void main() {
+                    gl_Position = a_Position;
+                }`;
+    const FS = `void main() {
+                    gl_FragColor = vec4(1, 0, 0, 1);
+                }`;
+
+    var vshader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vshader, VS);
+    gl.compileShader(vshader);
+
+    var fshader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fshader, FS);
+    gl.compileShader(fshader);
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vshader);
+    gl.attachShader(program, fshader);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+
+    var buf = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+    var a_Position = gl.getAttribLocation(program, 'a_Position');
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    var data = new Float32Array([0, 0.2, -0.2, -0.2, 0.2, -0.2]);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
+
+function tick() {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+    console.log('111');
+    requestAnimationFrame(tick)
+}
+
